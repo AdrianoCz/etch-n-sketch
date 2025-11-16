366d0a90698e30e0a2c455d1fdfabb99bd8f7b6d
Adriano <235764285+AdrianoCz@users.noreply.github.com>
2025-11-16 11:36:56 -0300

    add choice between classic and draw mode and limit grid size

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const container = document.querySelector("#container")
 const btn = document.querySelector("button")    
 const form = document.querySelector("form")
+const etchRadio = document.querySelector("#etch")
+const drawRadio = document.querySelector("#draw") 
+
+let drawType;
+
 function createGrid(e){
-    
     container.innerHTML = ""
     const input = document.querySelector("input") ;
+    const etch = document.querySelector("#etch")
+
+    
 
     let n = Number(input.value);
-    
+    let squareSize = 60/n;
+   
+    if (etch.checked){
+        drawType = "mouseover"
+    } else{
+        drawType = "click"
+    }
+
+    if (n>100){
+        alert("grid too big");
+        throw new Error("grid bigger than 100x100");
+    }
+   
     for (let i = 0; i < n; i++) {
         const line = document.createElement("div");
         line.classList.add("line");
         container.appendChild(line)
     }
+   
     document.querySelectorAll(".line").forEach(element => {
         for(i=0;i<n;i++){
             const block = document.createElement("div")
@@ -20,13 +40,22 @@ function createGrid(e){
             block.classList.add("selected-block");
             block.classList.toggle("selected-block")
             element.appendChild(block)
+            
     }  
     })
+    
     document.querySelectorAll(".block").forEach(element => {
-        element.addEventListener("click", () => {element.classList.toggle("selected-block")})
+        element.addEventListener(drawType, () => {element.classList.toggle("selected-block")})
+        element.style.height = `${squareSize}vh`
+        element.style.width =  `${squareSize}vh` 
     });
+   
     container.classList.add("appear")
+   
     e.preventDefault()
 }
+
 btn.addEventListener("click", createGrid)
 container.addEventListener("animationend", () => {container.classList.remove("appear")})
+etchRadio.addEventListener("click", () => {createGrid()})
+drawRadio.addEventListener("click", () => {createGrid()})
